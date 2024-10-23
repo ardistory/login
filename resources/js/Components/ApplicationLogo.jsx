ddd543dcf26d6c25577d5c869dc2e6fa211f97eb
Ardiansyah Putra <putraardiansyah530@gmail.com>
2024-10-23 16:18:10 +0700

    Fix: Remove Animation ApplicationLogo

--- resources/js/Components/ApplicationLogo.jsx
+++ resources/js/Components/ApplicationLogo.jsx
@@ -4,7 +4,7 @@ import { WifiHigh } from '@phosphor-icons/react';
 export default function ApplicationLogo() {
     return (
         <Link href={route('home')} className={'text-3xl md:text-6xl font-bold italic tracking-tight flex items-center justify-center gap-2 text-white mb-4'}>
-            <WifiHigh className={'animate-pulse'} />
+            <WifiHigh />
             Network Lebak
         </Link>
     );
